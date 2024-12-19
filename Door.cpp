21a4b62a7d77c7d2bd3bf359b85ce91d3f8fb706
Nawal Khawar <nawal.khawar@gmail.com>
2024-12-19 11:54:59 +0500

    Initial commit - added project files

---
+++ Door.cpp
@@ -0,0 +1,30 @@
+#include "Door.h"
+
+Door::Door()
+{
+	isOpen = false;
+}
+
+//return door state
+bool Door::checkIfOpen()
+{
+	if (isOpen) //door is open
+	{
+		return true;
+	}
+	else //door is closed
+	{
+		return false;
+	}
+}
+
+//open/close the door
+void Door::toggle()
+{
+	isOpen = !isOpen;
+}
+
+Door::~Door()
+{
+
+}
